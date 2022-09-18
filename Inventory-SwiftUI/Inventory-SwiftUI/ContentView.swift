d1a004d3b6126fb4308561245352376aca6b6c98
Safar Safarov <Safarov.safar@hotmail.com>
2022-09-18 17:24:47 +0500

    ✨

--- Inventory-SwiftUI/Inventory-SwiftUI/ContentView.swift
+++ Inventory-SwiftUI/Inventory-SwiftUI/ContentView.swift
@@ -4,18 +4,37 @@
 //
 //  Created by Safar Safarov on 18/09/22.
 //
-
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    @FirestoreQuery(collectionPath: "inventories") private var items: [InventoryItem]
+    private let db = Firestore.firestore().collection("inventories")
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if let error = $items.error {
+                Text(error.localizedDescription)
+            }
+            
+            if items.count > 0 {
+                List(items) { item in
+                    TextField("Name", text: Binding<String>(
+                        get: { item.name },
+                        set: { updateItem(item, data: ["name": $0]) }))
+                }
+            }
         }
-        .padding()
+    }
+    
+    private func updateItem(_ item: InventoryItem, data: [String: Any]) {
+        guard let id = item.id else { return }
+        var _data = data
+        _data["updatedAt"] = FieldValue.serverTimestamp()
+        db.document(id).updateData(_data)
     }
 }
 
