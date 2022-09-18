60a9fddafe29f586e38b68d2bb0e14b8c66378eb
Safar Safarov <Safarov.safar@hotmail.com>
2022-09-18 13:11:49 +0500

    ✨

---
+++ Inventory-SwiftUI/Inventory-SwiftUI/Inventory_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Inventory_SwiftUIApp.swift
+//  Inventory-SwiftUI
+//
+//  Created by Safar Safarov on 18/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Inventory_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
